d503778ee18d54b4bf3f7a8a61e83f00d54fe956
Aviv Komemi <komemiav@gmail.com>
2020-02-01 01:04:08 +0200

    Move player with particles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,10 @@ using Utilities;
 public class Player : MonoBehaviour
 {
     [SerializeField] Transform[] playerPositions;
+    [SerializeField] ParticleSystem teleportOut;
+    [SerializeField] ParticleSystem teleportIn;
+    [SerializeField] GameObject spriteObj;
+    DancePadKey lastKeyPressed;
 
     void Update()
     {
@@ -17,43 +21,89 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown((KeyCode)DancePadKey.MiddleLeft) || Input.GetKeyDown(KeyCode.A))
         {
-            transform.position = playerPositions[0].position;
+            if (lastKeyPressed != DancePadKey.MiddleLeft)
+            {
+                StartCoroutine(Move(0));
+                lastKeyPressed = DancePadKey.MiddleLeft;
+            }
             GameManager.Instance.ShieldMap[0].Repair();
+
         }
         if (Input.GetKeyDown((KeyCode)DancePadKey.BottomMiddle) || Input.GetKeyDown(KeyCode.X))
         {
-            transform.position = playerPositions[1].position;
+            if (lastKeyPressed != DancePadKey.BottomMiddle)
+            {
+                StartCoroutine(Move(1));
+                lastKeyPressed = DancePadKey.BottomMiddle;
+            }
             GameManager.Instance.ShieldMap[1].Repair();
         }
         if (Input.GetKeyDown((KeyCode)DancePadKey.TopMiddle) || Input.GetKeyDown(KeyCode.W))
         {
-            transform.position = playerPositions[2].position;
+            if (lastKeyPressed != DancePadKey.TopMiddle)
+            {
+                StartCoroutine(Move(2));
+                lastKeyPressed = DancePadKey.TopMiddle;
+            }
             GameManager.Instance.ShieldMap[2].Repair();
         }
         if (Input.GetKeyDown((KeyCode)DancePadKey.MiddleRight) || Input.GetKeyDown(KeyCode.D))
         {
-            transform.position = playerPositions[3].position;
+            if (lastKeyPressed != DancePadKey.MiddleRight)
+            {
+                StartCoroutine(Move(3));
+                lastKeyPressed = DancePadKey.MiddleRight;
+            }
             GameManager.Instance.ShieldMap[3].Repair();
         }
         if (Input.GetKeyDown((KeyCode)DancePadKey.BottomLeft) || Input.GetKeyDown(KeyCode.Z))
         {
-            transform.position = playerPositions[4].position;
+            if (lastKeyPressed != DancePadKey.BottomLeft)
+            {
+                StartCoroutine(Move(4));
+                lastKeyPressed = DancePadKey.BottomLeft;
+            }
             GameManager.Instance.ShieldMap[4].Repair();
         }
         if (Input.GetKeyDown((KeyCode)DancePadKey.BottomRight) || Input.GetKeyDown(KeyCode.C))
         {
-            transform.position = playerPositions[5].position;
+            if (lastKeyPressed != DancePadKey.BottomRight)
+            {
+                StartCoroutine(Move(5));
+                lastKeyPressed = DancePadKey.BottomRight;
+            }
             GameManager.Instance.ShieldMap[5].Repair();
         }
         if (Input.GetKeyDown((KeyCode)DancePadKey.TopLeft) || Input.GetKeyDown(KeyCode.Q))
         {
-            transform.position = playerPositions[6].position;
+            if (lastKeyPressed != DancePadKey.TopLeft)
+            {
+                StartCoroutine(Move(6));
+                lastKeyPressed = DancePadKey.TopLeft;
+            }
             GameManager.Instance.ShieldMap[6].Repair();
+
         }
         if (Input.GetKeyDown((KeyCode)DancePadKey.TopRight) || Input.GetKeyDown(KeyCode.E))
         {
-            transform.position = playerPositions[7].position;
+            if (lastKeyPressed != DancePadKey.TopRight)
+            {
+                StartCoroutine(Move(7));
+                lastKeyPressed = DancePadKey.TopRight;
+            }
             GameManager.Instance.ShieldMap[7].Repair();
         }
+    }
+
+    IEnumerator Move(int index)
+    {
+        teleportOut.Play();
+        yield return new WaitForSeconds(0.2f); 
+        spriteObj.SetActive(false);
+        yield return null;
+        transform.position = playerPositions[index].position;
+        teleportIn.Play();
+        yield return new WaitForSeconds(0.2f);
+        spriteObj.SetActive(true);
     }
 }
