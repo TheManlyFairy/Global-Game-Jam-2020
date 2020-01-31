8aef0f60b7390e8b2ccd7fd19933ed5ee24099fe
Aviv Komemi <komemiav@gmail.com>
2020-01-31 02:27:21 +0200

    Basic Concept
    Some test scripts and mechanics to prove the validity of the concept.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Enemy : MonoBehaviour
+{
+    public Transform enemyTarget;
+    public float moveSpeed = 5;
+    Rigidbody2D rigBody;
+    Vector3 startPos;
+    
+    private void Start()
+    {
+        startPos = transform.position;
+        rigBody = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        rigBody.MovePosition(rigBody.position + ((Vector2)(enemyTarget.position)- (Vector2)(transform.position)) * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        transform.position = startPos;
+    }
+}
