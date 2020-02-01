1d50e5fb3a0df6df791c7fb245e5fa9e1f64ef57
Aviv Komemi <komemiav@gmail.com>
2020-02-01 15:04:11 +0200

    particles and animations

---
+++ Assets/AnimateRuneLetters.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateRuneLetters : MonoBehaviour
+{
+    [SerializeField] float animateTime;
+    [SerializeField] Sprite[] runeLetterSprites;
+    float timer;
+    int spriteIndex=0;
+    SpriteRenderer spriteRend;
+
+    private void Start()
+    {
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer> animateTime)
+        {
+            timer = 0;
+            spriteIndex++;
+            if (spriteIndex == runeLetterSprites.Length)
+                spriteIndex = 0;
+            spriteRend.sprite = runeLetterSprites[spriteIndex];
+        }
+    }
+}
